e56b6043cd95ca20a7f98d38d9a7d2c2d57fc4f6
尾原徳泰 <nori.tukareta@gmail.com>
2022-05-30 09:50:22 +0900

    RxSwiftWeatherAPIViewModel実装

--- RxSwiftWeatherAPISample/RxSwiftWeatherAPISample/ViewModel.swift
+++ RxSwiftWeatherAPISample/RxSwiftWeatherAPISample/ViewModel.swift
@@ -8,3 +8,25 @@
 import Foundation
 import RxSwift
 import RxCocoa
+
+protocol RxSwiftWeatherAPIViewModelInput {
+    
+}
+
+
+protocol RxSwiftWeatherAPIViewModelOutput {
+    
+}
+
+protocol RxSwiftWeatherAPIViewModelType {
+    
+}
+
+
+final class protocol RxSwiftWeatherAPIViewModel: RxSwiftWeatherAPIViewModelInput, RxSwiftWeatherAPIViewModelOutput, RxSwiftWeatherAPIViewModelType {
+    
+}
+ {
+    
+}
+
